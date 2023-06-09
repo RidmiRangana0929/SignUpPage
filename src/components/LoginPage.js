0ee7af0ffc4169db5e053d6625e144847880d697
RidmiRangana0929 <ridmirangana1429@gmail.com>
2023-06-09 13:59:11 +0530

    added text fields

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -8,6 +8,25 @@ const LoginPage = () => {
         <div>
           <h2 className='title'>Student Login</h2>
         </div>
+
+        <form className='form-wrapper'>
+
+          <div className='name'>
+            <label className='lable'>Full Name</label>
+            <input className='input' type='text'/>
+          </div>
+
+          <div className='email'>
+            <label className='lable'>Email</label>
+            <input className='input' type='email'/>
+          </div>
+
+          <div className='password'>
+            <label className='lable'>Password</label>
+            <input className='input' type='password'/>
+          </div>
+
+        </form>
       </div>
 
     </div>
