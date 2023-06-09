b110e5e4bdbcc55c888656a5a9c9a169209d8109
RidmiRangana0929 <ridmirangana1429@gmail.com>
2023-06-09 21:28:03 +0530

    updated

--- src/components/SignUpPage.js
+++ src/components/SignUpPage.js
@@ -4,11 +4,13 @@ import React, { useState } from 'react'
 const SignUpPage = () => {
 
   const [fullName,setFullName] = useState("");
+  const [grade,setGrade] = useState("");
   const [email,setEmail] = useState("");
+  const [username,setUsername] = useState("");
   const [password,setPassword] = useState("");
 
   const handleFormSubmit = (e) => {
-    console.log("Full name : "+fullName," , "+"Email : "+email," , "+"Password : "+password);
+    console.log("Full name : "+fullName+" , "+"Grade : "+grade+" , "+"Email : "+email+" , "+"Username : "+username+" , "+"Password : "+password);
     e.preventDefault();
   }
 
@@ -25,22 +27,43 @@ const SignUpPage = () => {
 
           <div className='fullName'>
             <label className='lable'>Full Name</label>
-            <input className='input' 
+            <input className='input'
+            id='s_fullName' 
             type='text' 
             name='fullname' 
             onChange={ (e) => {setFullName(e.target.value)}}/>
           </div>
 
+          <div className='grade'>
+            <label className='lable'>Grade</label>
+            <input className='input' 
+            id='s_grade'
+            type='grade' 
+            name='grade' 
+            onChange={ (e) => {setGrade(e.target.value)}}/>
+          </div>
+
           <div className='email'>
             <label className='lable'>Email</label>
-            <input className='input' 
+            <input className='input'
+            id='s_email' 
             type='email' name='email' 
             onChange={ (e) => {setEmail(e.target.value)}}/>
+          </div>
+
+          <div className='username'>
+            <label className='lable'>Username</label>
+            <input className='input' 
+            id='s_username'
+            type='username' 
+            name='username' 
+            onChange={ (e) => {setUsername(e.target.value)}}/>
           </div>
 
           <div className='password'>
             <label className='lable'>Password</label>
             <input className='input' 
+            id='s_password'
             type='password' 
             name='password' 
             onChange={ (e) => {setPassword(e.target.value)}}/>
